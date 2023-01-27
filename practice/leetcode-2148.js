bf8f00600d35e222cc16a613facd0d869884a78a
ZeraiGR <kirillgr15@mail.ru>
2023-01-27 23:20:43 +0300

    solved 2148 problem

---
+++ practice/leetcode-2148.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/count-elements-with-strictly-smaller-and-greater-elements/
+import { assert } from "../tester.js";
+
+// Tags: Array, Sorting
+
+// O(N^2)
+const countElements = function(nums) {
+	nums.sort((a, b) => a - b);
+	let ans = 0;
+
+	if (nums.length > 2) {
+		for (let i = 1; i < nums.length - 1; ++i) {
+			let hasLess = nums.some(n => n < nums[i]);
+			let hasMore = nums.some(n => n > nums[i]);
+			if (hasLess && hasMore) ans++;
+		}
+	}
+
+	return ans;
+};
+
+assert(1, countElements([11,7,2,15]), 2);
+assert(2, countElements([-3,3,3,90]), 2);
+assert(3, countElements([1,1,2,3]), 1);
+assert(4, countElements([1,1,2,2]), 0);
+assert(5, countElements([1]), 0);
+assert(6, countElements([1, 2]), 0);
+assert(7, countElements([1, 2, 3]), 1);
