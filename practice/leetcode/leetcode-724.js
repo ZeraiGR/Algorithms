024ffef2ff9151da8f5e8e8a31616f19980ec1a2
ZeraiGR <kirillgr15@mail.ru>
2023-01-31 23:39:21 +0300

    leetcode problem 724 solved

---
+++ practice/leetcode/leetcode-724.js
@@ -0,0 +1,26 @@
+// Find Pivot Index
+import { assert } from "../../tester.js";
+// https://leetcode.com/problems/find-pivot-index/?envType=study-plan&id=level-1
+
+// Tags: Array, Prefix Sum
+
+const pivotIndex = function(nums) {
+  let left = 0;
+	let right = nums.slice(1).reduce((a,b) => a+b, 0);
+	
+	if (left === right) return 0;
+
+	for (let i = 1; i < nums.length; i++) {
+		left += nums[i-1];
+		right -= nums[i];
+		if (left === right) return i;
+	}
+
+	return -1;
+};
+
+assert(1, pivotIndex([2,1,-1]), 0);
+assert(2, pivotIndex([1,7,3,6,5,6]), 3);
+assert(3, pivotIndex([1,2,3]), -1);
+
+
