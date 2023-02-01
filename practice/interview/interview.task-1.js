eecda4e9f52cb14a56340a7438ea566e2b1ff3eb
ZeraiGR <kirillgr15@mail.ru>
2023-02-01 12:09:49 +0300

    added one more solution to interview.task-1

--- practice/interview/interview.task-1.js
+++ practice/interview/interview.task-1.js
@@ -31,3 +31,42 @@ function parsePhones (clients) {
 }
 
 console.log(parsePhones(clients));
+
+
+// Level 2
+
+const clients2 = [
+	[
+		{name: 'Vasya', phone: '333'},
+		{name: 'Kirill', phone: '444'},
+		{name: 'Ivan', phone: '555'},
+	],
+	[
+		{name: 'Anton', phone: '333'},
+		{name: 'Anatoliy', phone: '444'},
+		{name: 'Arseniy', phone: '666'}
+	]
+];
+
+function myReduce(collection, callback, init) {
+	let acc = init;
+	for (const item of collection) {
+		acc = callback(acc, item);
+	}
+	return acc;
+}
+
+function parsePhones2 (clients) {
+	const parsedObj = myReduce(clients.flat(), (acc, {name, phone}) => {
+		if (!Object.prototype.hasOwnProperty.call(acc, phone)) {
+			acc[phone] = [name];
+		} else {
+			acc[phone].push(name);
+		}
+		return acc;
+	}, {});
+
+	return Object.fromEntries(Object.entries(parsedObj).filter(([_, usrs]) => usrs.length > 1));
+}
+
+console.log(parsePhones2(clients2));
