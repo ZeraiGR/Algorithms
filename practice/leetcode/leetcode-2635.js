626d0208e2fdc0ace9f83fd97dad285c89b9fd07
ZeraiGR <kirillgr15@mail.ru>
2023-04-12 23:46:31 +0300

    leetcode problem 2635 was solved

---
+++ practice/leetcode/leetcode-2635.js
@@ -0,0 +1,15 @@
+// Apply Transform Over Each Element in Array
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/description/
+
+const map = function(arr, fn) {
+  let res = [];
+  
+  let i = 0;
+  
+  for (el of arr) {
+      res.push(fn(+el, i, arr));
+      i += 1;
+  }
+
+  return res;
+};
