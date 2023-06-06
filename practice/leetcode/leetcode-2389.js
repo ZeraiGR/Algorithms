c9419b2af76a3cd68bca72d0a69c918592ac10b3
ZeraiGR <kirillgr15@mail.ru>
2023-06-06 23:57:38 +0300

    leetcode problem 2389 was started

---
+++ practice/leetcode/leetcode-2389.js
@@ -0,0 +1,29 @@
+// Longest Subsequence With Limited Sum
+// https://leetcode.com/problems/longest-subsequence-with-limited-sum/
+
+// Tags: Array, Binary Search, Greedy, Sorting, Prefix Sum
+
+const answerQueries = function(nums, queries) {
+  let res = [];
+  let sortedNums = [...nums].sort((a, b) => a - b);
+  
+
+  for (let i = 0; i < queries.length; i++) {
+    let curQuery = queries[i];
+    let cur = 0;
+    let 
+    
+    for (let j = 0; j < sortedNums.length; j++) {
+      cur += sortedNums[j];
+
+      if (cur > curQuery) break;
+
+
+    }
+
+    res.push(cur);
+  }
+  return res;
+};
+
+console.log(answerQueries([4,5,2,1], [3,10,21]));
