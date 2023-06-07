7a7ed21053d387a7f122357c17897e1f258a89d1
ZeraiGR <kirillgr15@mail.ru>
2023-06-07 13:25:17 +0300

    leetcode problem 2389 was solved

--- practice/leetcode/leetcode-2389.js
+++ practice/leetcode/leetcode-2389.js
@@ -3,6 +3,7 @@
 
 // Tags: Array, Binary Search, Greedy, Sorting, Prefix Sum
 
+// Brute force
 const answerQueries = function(nums, queries) {
   let res = [];
   let sortedNums = [...nums].sort((a, b) => a - b);
@@ -26,3 +27,51 @@ const answerQueries = function(nums, queries) {
 };
 
 console.log(answerQueries([4,5,2,1], [3,10,21]));
+
+// Prefix sum + Binary Search
+const answerQueries2 = function(nums, queries) {
+  // 1 - sorting
+  const sortedNums = [...nums].sort((a, b) => a - b);
+
+  // 2 - create prefix sum
+  const numsPS = makePrefixSum(sortedNums);
+
+  // 3 - use binary search
+  let ans = [];
+  for (let i = 0; i < queries.length; i++) {
+    const query = queries[i];
+    ans.push(binSearch(0, numsPS.length - 1, numsPS, query) + 1);
+  }
+
+  return ans;
+}
+
+function binSearch (l, r, arr, query) {
+  if (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+
+    if (arr[mid] === query) {
+      return mid;
+    } else if (arr[mid] > query) {
+      return binSearch(l, mid - 1, arr, query);
+    } else {
+      return binSearch(mid + 1, r, arr, query);
+    }
+  } else { 
+    return r;
+  }
+}
+
+function makePrefixSum (arr) {
+  let sum = [];
+  let cur = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    cur += arr[i];
+    sum.push(cur);
+  }
+
+  return sum;
+}
+
+console.log(answerQueries2([4,5,2,1], [3,10,21]));
