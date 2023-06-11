341c038a21a0241319fcd871aa60838b33167f66
ZeraiGR <kirillgr15@mail.ru>
2023-06-12 00:03:24 +0300

    leetcode problem 121 was updated

--- practice/leetcode/leetcode-121.js
+++ practice/leetcode/leetcode-121.js
@@ -21,3 +21,43 @@ const maxProfit = function(prices) {
 console.log('1 ', maxProfit([7,1,5,3,6,4])); // 5
 console.log('2 ', maxProfit([7,6,4,3,1])); // 0
 console.log('3 ', maxProfit([2,4,1])); // 2
+
+const maxProfit2 = function (prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    let curPrice = prices[i];
+
+    if (curPrice < minPrice) {
+      minPrice = curPrice;
+    } else {
+      let curProfit = curPrice - minPrice;
+
+      if (curProfit > maxProfit) {
+        maxProfit = curProfit;
+      }
+
+    }
+  }
+
+  return maxProfit;
+}
+
+console.log('1 ', maxProfit2([7,1,5,3,6,4])); // 5
+console.log('2 ', maxProfit2([7,6,4,3,1])); // 0
+console.log('3 ', maxProfit2([2,4,1])); // 2
+
+const maxProfit3 = function (prices) {
+  let maxProfit = 0;
+  let minPrice = prices[0];
+
+  for (let i = 0; i < prices.length; i++) {
+    let price = prices[i];
+
+    maxProfit = Math.max(maxProfit, price - minPrice);
+    minPrice = Math.min(minPrice, price);
+  }
+
+  return maxProfit;
+}
