2287cccf19d0ad5ad690a79a0a1b033b076c6747
ZeraiGR <kirillgr15@mail.ru>
2023-06-14 23:56:18 +0300

    leetcode problem 2703 was solved

---
+++ practice/leetcode/leetcode-2703.js
@@ -0,0 +1,8 @@
+// Return Length of Arguments Passed
+// https://leetcode.com/problems/return-length-of-arguments-passed/description/
+
+// Tags: Frontend (javaScript)
+
+const argumentsLength = function(...args) {
+  return args.length;
+};
