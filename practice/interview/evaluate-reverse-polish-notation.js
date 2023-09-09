0fbe53cb7945a8e1447493c5a0d6a45d59d41949
ZeraiGR <kirillgr15@mail.ru>
2023-09-09 17:31:24 +0300

    evaluate reverse polish notation was solved

---
+++ practice/interview/evaluate-reverse-polish-notation.js
@@ -0,0 +1,42 @@
+// Evaluate Reverse Polish Notation
+
+/*
+
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+*/
+
+const evalRPN = function(tokens) {
+  const stack = [];
+
+  for (const token of tokens) {
+    if (token === '+') {
+      stack.push(stack.pop() + stack.pop());
+    } else if (token === '-') {
+      let a = stack.pop();
+      let b = stack.pop();
+      stack.push(b - a);
+    } else if (token === '*') {
+      stack.push(stack.pop() * stack.pop());
+    } else if (token === '/') {
+      let a = stack.pop();
+      let b = stack.pop();
+      stack.push(Math.trunc(b / a));
+    } else {
+      stack.push(Number(token));
+    }
+  }
+
+  return stack[0];
+};
